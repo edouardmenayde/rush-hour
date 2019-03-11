5471f5d613bf31f5030ea3012d143656fb7571ef
Edouard Menayde <edouard.menayde@protonmail.com>
2019-03-11 12:43:07 +0100

    Give default values to car fields

--- src/Car.h
+++ src/Car.h
@@ -7,10 +7,10 @@ enum Direction {
 };
 
 struct Car {
-  unsigned int line;
-  unsigned int column;
-  unsigned int length;
-  Direction direction;
+  unsigned int line = 0;
+  unsigned int column = 0;
+  unsigned int length = 0;
+  Direction direction = HORIZONTAL;
 
   /**
    * Determines if the car is the one we need to the one we need to get out of the Situation.
